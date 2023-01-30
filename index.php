93422e75e8f0fe5fce6226f27b9e5ef25f27fa32
Stiven <91064676+dm12stiven@users.noreply.github.com>
2023-01-30 12:32:59 +0300

    Update index.php

--- index.php
+++ index.php
@@ -15,19 +15,19 @@ file_put_contents('file.txt','$data: '.print_r($subdata,1)."\n", FILE_APPEND);
 
 $data = $data['callback_query'] ? ['callback_query'] : $data['message'];
 
-define('TOKEN','5430451748:AAH5Enbn3YMXb-unGg5iJpSLKCNKG3wBuMM');
+define('TOKEN',' paste tg bot token here ');
 
 $message = mb_strtolower(($data['text'] ? $data['data'] : $data['data']),'utf-8');
 
 // echo 'STI_Bot<br><br>';
 // var_dump($data);
-// https://api.telegram.org/bot5430451748:AAH5Enbn3YMXb-unGg5iJpSLKCNKG3wBuMM/sendMessage?chat_id=651492048&parse_mode=HTML&text=%3Ci%3E%D0%9F%D1%80%D0%B8%D0%B2%D0%B5%D1%82,%20%D0%94%D0%B8%D0%BC%D0%B0%3C/i%3E
+
 
 function sendmsg($msg)
 {
     $myCurl = curl_init();
     curl_setopt_array($myCurl, array(
-        CURLOPT_URL => 'https://api.telegram.org/bot5430451748:AAH5Enbn3YMXb-unGg5iJpSLKCNKG3wBuMM/sendMessage?chat_id=651492048&parse_mode=HTML&text='. $msg,
+        CURLOPT_URL => 'https://api.telegram.org/bot'.TOKEN.'/sendMessage?chat_id=651492048&parse_mode=HTML&text='. $msg,
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_POST => true
     ));
